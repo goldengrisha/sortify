d7d4976875614c027d7e44623adebe3c2c428642
Gregory <goldengrisha@gmail.com>
2024-06-22 19:18:11 +0300

    cleaned up a bit

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -8,4 +8,4 @@ def get_tickets_as_df(path: str = "./data/tickets.csv") -> pd.DataFrame:
 def get_cleaned_tickets_as_df(
     path: str = "./data/cleaned_tickets_v1.csv",
 ) -> pd.DataFrame:
-    return pd.read_csv(path, date_parser=["ticket_created_on", "message_created_on"])
+    return pd.read_csv(path)
